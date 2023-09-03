ea6566333cbc105c43962ee95497357f57dcd446
github.com/nicolyy <nicollyoliveira003@gmail.com>
2023-09-03 19:57:53 -0300

    Ajustando a main

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 
-
-//BLOCO DE ROTAS
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './routes/Home/index.jsx';
-import Produtos from './routes/Produtos/index.jsx';
-import EditarProdutos from './routes/EditarProdutos/index.jsx';
-import ExcluirProdutos from './routes/ExcluirProdutos/index.jsx';
-import Erro404 from './routes/Erro404/index.jsx';
-
-
-//Criando o objeto de rotas
+// Rotas
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
+import Home from './routes/Home/index.jsx'
+import Produtos from './routes/Produtos/index.jsx'
+import Error from './routes/Error/index.jsx'
+import EditarProduto from './routes/EditarProdutos/index'
+// Configurando as rotas
 const router = createBrowserRouter([
-  { path: '/', 
-      element: <App />,
-      errorElement: <Erro404 />, 
-      children: [
-        { path: '/', element: <Home /> },
-        { path: '/produtos', element: <Produtos /> },
-        { path: '/editar/produtos/:id', element: <EditarProdutos /> },
-        { path: '/excluir/produtos/:id', element: <ExcluirProdutos /> },
-      ]
-  },
+  {
+    path: '/',
+    element: <App/>,
+    errorElement: <Error/>,
+    children:[
+      {
+        path: '/',
+        element: <Home/>
+      },
+      {
+        path: '/produtos',
+        element: <Produtos/>
+      },
+      {
+        path: '/produtos/editar/:id',
+        element: <EditarProduto/>
+      },
+      {
+        path: '/antiga',
+        element: <Navigate to='/'/>
+      }
+    ]
+  }
 ])
-
-//BLOCO DE ROTAS
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
