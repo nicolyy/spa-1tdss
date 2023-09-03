bf6a694080ac8f6e9382025f9b74ff6bd8268e3f
github.com/nicolyy <nicollyoliveira003@gmail.com>
2023-09-03 12:32:12 -0300

    Editando o app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Outlet } from "react-router-dom";
+import Cabecalho from "./Components/Cabecalho";
+import Rodape from "./Components/Rodape";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* Crie um Cabeçalho com um h1 um título e uma lista com 3 itens. */}
+      <Cabecalho />
+      {/* Crie uma Section com uma div e 3 parágrafos com texto lorem de 3 linhas. */}
+      {/* Passando três props para o componente filho. */}
+      <Outlet />
+      {/* Crie um rodapé com um p e o código do símbolo de copyright mais o texto que se segue. "Todos os meus direitos reservados. 2023" */}
+      <Rodape />
     </>
-  )
+  );
 }
-
-export default App
