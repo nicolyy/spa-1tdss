51b4d4c96dfc34373e6122c3e7e2f0e7e6bf5f94
Nicoly Oliveira <89228754+nicolyy@users.noreply.github.com>
2023-09-05 10:51:25 -0300

    Update index.jsx

--- src/routes/Produtos/index.jsx
+++ src/routes/Produtos/index.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { listaProdutos } from "../../components/ListaProdutos";
+import { ListaProdutos } from "../../components/listaProdutos";
 
 export default function Produtos() {
   
